6da7ff99c379ac3dd0ec98278ee7e8e679381b12
Ayxan <45293675+Ayxan13@users.noreply.github.com>
2020-05-04 17:14:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,24 @@
 
 int main()
 {
-	GamePlay gamePlay;
-	gamePlay.play();
+	while (true) {
+		GamePlay gamePlay;
+		gamePlay.play();
+		
+		sleep(100);
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Press Q to quit. Press C to continue\n";
+		
+		int pressed;
+		while ( (pressed = get_pressed_key()) != 'q' && pressed != 'c') {
+			std::cout << "Press either Q or C\n";
+		}
+		
+		if (pressed == 'q') 
+			break;
+		clear_scrn();
+	}
+	
+	
 	return 0;
 }
